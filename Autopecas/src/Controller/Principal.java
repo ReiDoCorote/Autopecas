3d34504f49fa3cfc0d2a3de677b2aef4cfbc024d
Reginaldo Junior <85908511+radjunior@users.noreply.github.com>
2021-12-14 18:36:17 -0300

    4° Sprint atualização 1.1
    >Correção do ViewLogin

--- Autopecas/src/Controller/Principal.java
+++ Autopecas/src/Controller/Principal.java
@@ -6,7 +6,7 @@ import View.ViewPrincipal;
 public class Principal {
     public static void main(String args[]) {
         ViewLogin vlg = new ViewLogin();
-       // vlg.setVisible(true);
+        vlg.setVisible(true);
         ViewPrincipal vPri = new ViewPrincipal();
         vPri.setVisible(true);
     }
